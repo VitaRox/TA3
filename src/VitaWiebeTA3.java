a6a8c2344203eaac7ee639761aac3466ef6f625d
VitaRox <vita.wiebe@seattlecolleges.edu>
2019-10-23 16:37:44 -0700

    - initial commit: layout out basic program design;

---
+++ src/VitaWiebeTA3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+ *@author Vita Wiebe;
+ *@version Assignment 3, Tuesday class, section H9;
+ */
+public class VitaWiebeTA3 {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to parse user input;
+        Scanner scan = new Scanner(System.in);
+
+        // Create counter to stop scanner from checking for input eternally;
+        int count = 0;
+
+        // Create an array to hold user input, 7 spaces
+        int[] userIn = new int[7];
+
+        System.out.println("Please enter 7 integers, each separated by one space: ");
+        while (count < 6){
+            int current = scan.nextInt();
+
+            count++;
+        }
+    }
+
+
+
+}

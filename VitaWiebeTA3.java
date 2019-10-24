399acaea8d12deb73fc7b6102b0d7d43b1b94f32
VitaRox <Vita.Wiebe@seattlecolleges.edu>
2019-10-24 16:17:09 -0700

    Add files via upload
    - Initial online commit;

---
+++ VitaWiebeTA3.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.math.*;
+
+/*
+ *@author Vita Wiebe;
+ *@version Assignment 3, Tuesday class, section H9;
+ */
+public class VitaWiebeTA3 {
+
+    // Create a public recursive method, which will call its overloaded private helper method;
+    public static int getMax(int[] ourArray){
+        return getMax(ourArray, 0);
+     }
+
+    // Create a private recursive helper method with 2 params:
+    private static int getMax(int[] ourArray, int idx){
+
+        // Initialize max to the value
+        int max = ourArray[idx];
+
+        max = Math.max(max, ourArray[idx + 1]);
+        if(idx < ourArray.length){
+            getMax(ourArray, idx++);
+        }
+        return max;
+    }
+
+    // Our application method;
+    // Obtains user input and calls pertinent methods;
+    public static void main(String[] args) {
+
+        // Create Scanner object to parse user input;
+        Scanner scan = new Scanner(System.in);
+
+        // Create counter to stop scanner from checking for input eternally;
+        int count = 0;
+
+        // Create an array to hold user input, 7 spaces
+        int[] userIn = new int[7];
+
+        // Prompt end user for input of appropriate type;
+        System.out.println("Please enter 7 integers, each separated by one space: ");
+        while (count < 7){
+
+            // Assign each token (int) to a temp variable, "current";
+            int current = scan.nextInt();
+
+            // Add the current value to our array for storage;
+            userIn[count] = current;
+            count++;
+        }
+        // A for-each loop to test that user input is being taken and stored correctly;
+        for (int value : userIn) {
+            System.out.println(value);
+        }
+        System.out.println("the max is " + getMax(userIn));
+    }
+
+
+
+}
